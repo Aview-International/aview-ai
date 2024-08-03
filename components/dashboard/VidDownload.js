17b3457b5e48f1855783e356b8ab4d6c5e813bd3
d-huliu <huliud9vid@gmail.com>
2024-08-03 14:16:41 -0400

    finished yt-download first page

--- components/dashboard/VidDownload.js
+++ components/dashboard/VidDownload.js
@@ -6,11 +6,12 @@ const VidDownload = () => {
   const [videoLink, setVideoLink] = useState('');
 
   return (
-    <div className="w-full h-screen bg-white-transparent flex flex-col items-center justify-center">
-      <h1 className="text-white text-7xl font-bold mb-8">YouTube Video Downloader</h1>
+    <div className="w-full h-screen bg-white-transparent flex flex-col items-center">
+      <h1 className="text-white text-7xl font-bold mt-20 pt-20 ">YouTube Video Downloader</h1>
 
-      <div className="flex flex-row items-center justify-center">
-      <Border borderRadius="md" classes="w-1/3 mb-4">
+      <div className="flex flex-row items-center w-3/5 justify-center rounded-xl m-5">
+      <Border borderRadius="md" classes="w-2/3 mb-4 rounded-xl">
+      <div className="bg-black rounded-xl w-full h-full">
         <input
           type="text"
           placeholder="Paste video link here"
@@ -18,8 +19,9 @@ const VidDownload = () => {
           onChange={(e) => setVideoLink(e.target.value)}
           className="w-full p-4 bg-transparent text-white outline-none border-none"
         />
+        </div>
       </Border>
-      <div className="flex space-x-4">
+      <div className="flex m-5">
         <Button type="primary" onClick={() => console.log('Downloading...')}>
           Download
         </Button>
